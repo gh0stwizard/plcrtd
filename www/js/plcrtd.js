5929a3c5409c581bceea1bdf95dd8befe9a6cf3f
gh0stwizard <vitaliy.tokarev@gmail.com>
2015-07-02 14:32:27 +0300

    new behaviours system [js]

--- www/js/plcrtd.js
+++ www/js/plcrtd.js
@@ -10,71 +10,118 @@ $( function() {
       sizeOptions = [ 1024, 2048, 4096 ],
       pkOptions = [ 'RSA', 'DSA' ];
 
+
   function sortByName( a, b ) {
-    return ( a.name()() == b.name()() ) ? 0 : ( a.name()() < b.name()() ? -1 : 1 );
+    aName = $.isFunction( a.Name ) ? a.Name() : a.Name;
+    bName = $.isFunction( b.Name ) ? b.Name() : b.Name;
+
+    return ( aName == bName ) ? 0 : ( aName < bName ? -1 : 1 );
   }
 
-  function Database ( n, d ) {
-    var db = this;
 
-    db.name = ko.observable( n );
-    db.description = ko.observable( d );
-    db.isActive = ko.observable( false );
-    db.editing = ko.observable( false );
-    db.edit = function () {
-      this.editing( true );
-    };
-
-    return db;
+  function Database ( n = 'db', d = 'description' ) {
+    this.Name = ko.observable( n );
+    this.Description = ko.observable( d );
+    this.isActive = ko.observable( false );
+    this.editing = ko.observable( false );
+    this.edit = function () { this.editing( true ); }
   }
-  
+
   function DatabaseSetup ( name ) {
-    var setup = this;
-    
-    setup.dbname = name;
-    setup.capath = ko.observable( '/path/to/ca.key' );
-    
-    return setup;
+    this.Name = name;
+  }
+
+  function PrivateKey ( name = 'key', type = 'RSA', size = 2048 ) {
+    this.Name = ko.observable( name );
+    this.Type = ko.observable( type );
+    this.Size = ko.observable( size );
   }
 
 
-  function PrivateKey ( name, type, size ) {
-    var pk = this;
-    
-    pk.name = ko.observable( name );
-    pk.type = ko.observable( type );
-    pk.size = ko.observable( size );
-    
-    return pk;
-  }
-  
-  
-  function Request ( name ) {
-    var req = this;
-    
-    req.name = ko.observable( name );
-    
-    return req;
+  function Request ( name = 'csr' ) {
+    this.Name = ko.observable( name );
   }
 
   
-  function Certificate ( name ) {
-    var cert = this;
-    
-    cert.name = ko.observable( name );
-    
-    return cert;
+  function Certificate ( name = 'ca' ) {
+    this.Name = ko.observable( name );
   }
 
   
-  function RevocationList ( name ) {
-    var rl = this;
-    
-    rl.name = ko.observable( name );
-    
-    return rl;
+  function RevocationList ( name = 'crl' ) {
+    this.Name = ko.observable( name );
   }
 
+
+  function Page ( args = {} ) {
+    this.args = args;
+
+    this.onCreate = ko.observable( false );
+    this.onWipe = ko.observable( false );
+    this.onTable = ko.observable( false );
+    this.List = ko.observableArray( [ ] );
+    this.Item = ko.observable();
+    
+    this.CreateItem= args.CreateItem
+      ? args.CreateItem.bind( this )
+      : function ( ) { };
+      
+    function Create () {
+      this.List.push( this.Item() );
+      this.List.sort( sortByName );
+      this.CreateToggle();
+    }
+
+    this.Create = args.Create ? args.Create.bind( this ) : Create.bind( this );
+    
+    function Remove ( item ) {
+      this.List.remove( item );
+    }
+    
+    this.Remove = args.Remove ? args.Remove.bind( this ) : Remove.bind( this );
+    
+    function Wipe () {
+      this.List.removeAll();
+      this.WipeToggle();
+    }
+
+    this.Wipe   = args.Wipe   ? args.Wipe.bind( this )   : Wipe.bind( this );
+
+    function CreateToggle () {
+      this.onWipe( false );
+
+      if ( this.onCreate() ) {
+        this.onCreate( false );
+        this.onTable( true );
+        this.Item( null );
+      } else {
+        this.onCreate( true );
+        this.onTable( false );
+        this.Item( this.CreateItem() );
+      }
+
+      return false;
+    }
+
+    this.CreateToggle = CreateToggle.bind( this );
+
+    function WipeToggle () {
+      this.onCreate( false );
+
+      if ( this.onWipe() ) {
+        this.onWipe( false );
+        this.onTable( true );
+      } else {
+        this.onWipe( true );
+        this.onTable( false );
+      }
+
+      return false;
+    }
+    
+    this.WipeToggle = WipeToggle.bind( this );
+  }
+  
 
   function AppViewModel() {
     var self = this;
@@ -88,55 +135,11 @@ $( function() {
     /*  Data: show page toggles  */
 
     self.onAbout = ko.observable( false );
+    self.onConfigure = ko.observable( false );
     self.onPrivateKeys = ko.observable( false );
     self.onRequests = ko.observable( false );
     self.onCertificates = ko.observable( false );
     self.onRevoked = ko.observable( false );
-
-    /*  Data: Configure Page  */
-
-    self.onConfigure = ko.observable( false );
-    self.onCreateDB = ko.observable( false );
-    self.onWipeDBs = ko.observable( false );
-
-    /*  Data: Configure Page: DB management  */
-
-    self.DB = ko.observable();
-    self.DBList = ko.observableArray( [ ] );
-    self.onDBTable = ko.observable( false );
-    self.DBsetup = ko.observable();
-
-    /*  Data: Private Keys  */
-    
-    self.onCreatePK = ko.observable( false );
-    self.onWipePKs = ko.observable( false );
-    self.onPKTable = ko.observable( false );
-    self.PKList = ko.observableArray( [ ] );
-    self.PK = ko.observable();
-
-    /*  Data: Requests  */
-    
-    self.onCreateRQ = ko.observable( false );
-    self.onWipeRQs = ko.observable( false );
-    self.onRQTable = ko.observable( false );
-    self.RQList = ko.observableArray( [ ] );
-    self.RQ = ko.observable();
-
-    /*  Data: Certificates  */
-    
-    self.onCreateCR = ko.observable( false );
-    self.onWipeCRs = ko.observable( false );
-    self.onCRTable = ko.observable( false );
-    self.CRList = ko.observableArray( [ ] );
-    self.CR = ko.observable();
-
-    /*  Data: Revoked  */
-    
-    self.onCreateRL = ko.observable( false );
-    self.onWipeRLs = ko.observable( false );
-    self.onRLTable = ko.observable( false );
-    self.RLList = ko.observableArray( [ ] );
-    self.RL = ko.observable();
 
 
     /*  Behaviours  */
@@ -148,275 +151,91 @@ $( function() {
     self.Certificates = function () { location.hash = 'certificates'; }
     self.Revoked =      function () { location.hash = 'revoked';      }
 
+
     /*  Behaviours: Configuration  */
     
-    /* Create DB button toggle */
-    self.btnCreateDB = function () {
-      self.onWipeDBs( false );
-
-      if ( self.onCreateDB() ) {
-        self.onCreateDB( false );
-        self.onDBTable( true );
-        self.DB( null );
-      } else {
-        self.onCreateDB( true );
-        self.onDBTable( false );
-        self.DB( new Database( 'db', '' ) );
+    self.cfg = new Page( {
+      CreateItem : function () { return new Database(); },
+      Remove : function ( db ) {
+        this.Settings( null );
+        this.List.remove( db );
+      },
+      Wipe : function () {
+        this.Settings( null );
+        this.List.removeAll();
+        this.WipeToggle();
       }
-
-      return false;
-    }
-
-    /* Wipe DBs button toggle */
-    self.btnWipeDBs = function () {
-      self.onCreateDB( false );
-
-      if ( self.onWipeDBs() ) {
-        self.onWipeDBs( false );
-        self.onDBTable( true );
-      } else {
-        self.onWipeDBs( true );
-        self.onDBTable( false );
-      }
-
-      return false;
-    }
-
-    self.CreateDB = function () {
-      var db = self.DB(),
-          name = db.name,
-          desc = db.description;
-
-      self.DBList.push( new Database( name, desc ) );
-      self.DBList.sort( sortByName );
-      self.btnCreateDB();
-
-      return false;
-    }
-
-    self.UpdateDB = function ( ) {
-      /*  XXX  */
-      var name = this.name()(),
-          desc = this.description()();
-      
-      console.log( 'updating: ' + name );
-      
-    }
+    } );
     
-    self.RemoveDB = function ( db ) {
-      /*  XXX  */
-      var name = this.name()();
+    function ActivateDB ( db ) {
+      var name = db.Name();
       
-      console.log( 'removing: ' + name );
-      
-      self.DBList.remove( db );
-    }
-
-    self.WipeDBs = function () {
-      self.DBList.removeAll();
-      self.DBsetup( null );
-      self.btnWipeDBs();
-    }
-    
-    self.ActivateDB = function ( db ) {
-      var name = db.name()();
-      console.log( 'activating: ' + name );
-      self.DBsetup( new DatabaseSetup( name ) );
-      
-      var dbs = self.DBList(),
+      var dbs = this.List(),
           len = dbs.length;
 
+      /* mark all as in-active */
       for ( var i = 0; i < len; i++ ) {
         dbs[i].isActive( false );
       }
       
-      db.isActive( true );
-    }
-    
-    self.SetupDB = function () {
-      var setup = self.DBsetup(),
-          name = setup.dbname;
-
-      console.log( 'setupdb, db = ' + name );
+      /* activate choosen db */
+      db.isActive( true );      
       
-      return false;
+      /* retrieve settings */
+      this.Settings( new DatabaseSetup( name ) );
     }
     
+    function SetupDB ( ) {
+      /* TODO */
+    }
+
+    $.extend( self.cfg, {
+      Activate: ActivateDB.bind( self.cfg ),
+      Settings: ko.observable(),
+      Setup: SetupDB.bind( self.cfg )
+    } );
+
     
     /*  Behaviours: Private Keys  */
-    
-    self.btnCreatePK = function () {
-      self.onWipePKs( false );
 
-      if ( self.onCreatePK() ) {
-        self.onCreatePK( false );
-        self.onPKTable( true );
-        self.PK( null );
-      } else {
-        self.onCreatePK( true );
-        self.onPKTable( false );
-        self.PK( new PrivateKey( 'key', 'RSA', 1024 ) );
+    self.pk = new Page( {
+      CreateItem : function () { return new PrivateKey(); },
+/* TODO
+      Create : function () {
+        this.List.push( this.Item() );
+        this.List.sort( sortByName );
+        this.CreateToggle();
+      },
+      Remove : function ( pk ) {
+        this.List.remove( pk );
+      },
+      Wipe : function () {
+        this.List.removeAll();
+        this.WipeToggle();
       }
-
-      return false;    
-    }
-    
-    self.btnWipePKs = function () {
-      self.onCreatePK( false );
-
-      if ( self.onWipePKs() ) {
-        self.onWipePKs( false );
-        self.onPKTable( true );
-      } else {
-        self.onWipePKs( true );
-        self.onPKTable( false );
-      }
-
-      return false;    
-    }
-    
-    self.CreatePK = function () {
-      var pk = self.PK(),
-          name = pk.name,
-          type = pk.type,
-          size = pk.size;
-
-      self.PKList.push( new PrivateKey( name, type, size ) );
-      self.PKList.sort( sortByName );
-      self.btnCreatePK();
-
-      return false;    
-    }
-    
-    self.RemovePK = function ( pk ) {
-      /*  XXX  */
-      var name = this.name()();
-      
-      console.log( 'removing: ' + name );
-      
-      self.PKList.remove( pk );
-    }
-    
-    self.WipePKs = function () {
-      self.PKList.removeAll();
-      self.btnWipePKs();
-    }
+*/
+    } );
     
     
     /*  Behaviours: Requests  */
     
-    self.btnCreateRQ = function () {
-      self.onWipeRQs( false );
-
-      if ( self.onCreateRQ() ) {
-        self.onCreateRQ( false );
-        self.onRQTable( true );
-        self.RQ( null );
-      } else {
-        self.onCreateRQ( true );
-        self.onRQTable( false );
-        self.RQ( new Request( 'csr' ) );
-      }
-
-      return false;    
-    }
+    self.csr = new Page( {
+      CreateItem : function () { return new Request(); }
+    } );
     
-    self.btnWipeRQs = function () {
-      self.onCreateRQ( false );
-
-      if ( self.onWipeRQs() ) {
-        self.onWipeRQs( false );
-        self.onRQTable( true );
-      } else {
-        self.onWipeRQs( true );
-        self.onRQTable( false );
-      }
-
-      return false;    
-    }
-    
-    self.CreateRQ = function () {
-      var csr = self.RQ(),
-          name = csr.name;
-
-      self.RQList.push( new Request( name ) );
-      self.RQList.sort( sortByName );
-      self.btnCreateRQ();
-
-      return false;    
-    }
-    
-    self.RemoveRQ = function ( req ) {
-      /*  XXX  */
-      var name = this.name()();
-      
-      console.log( 'removing: ' + name );
-      
-      self.RQList.remove( req );
-    }
-    
-    self.WipeRQs = function () {
-      self.RQList.removeAll();
-      self.btnWipeRQs();
-    }
-
 
     /*  Behaviours: Certificates  */
     
-    self.btnCreateCR = function () {
-      self.onWipeCRs( false );
+    self.crt = new Page( {
+      CreateItem : function () { return new Certificate(); }
+    } );
 
-      if ( self.onCreateCR() ) {
-        self.onCreateCR( false );
-        self.onCRTable( true );
-        self.CR( null );
-      } else {
-        self.onCreateCR( true );
-        self.onCRTable( false );
-        self.CR( new Certificate( 'Request' ) );
-      }
 
-      return false;    
-    }
+    /*  Behaviours: Revocation Lists  */
     
-    self.btnWipeCRs = function () {
-      self.onCreateCR( false );
-
-      if ( self.onWipeCRs() ) {
-        self.onWipeCRs( false );
-        self.onCRTable( true );
-      } else {
-        self.onWipeCRs( true );
-        self.onCRTable( false );
-      }
-
-      return false;    
-    }
-    
-    self.CreateCR = function () {
-      var cert = self.CR(),
-          name = cert.name;
-
-      self.CRList.push( new Certificate( name ) );
-      self.CRList.sort( sortByName );
-      self.btnCreateCR();
-
-      return false;
-    }
-    
-    self.RemoveCR = function ( req ) {
-      /*  XXX  */
-      var name = this.name()();
-      
-      console.log( 'removing: ' + name );
-      
-      self.CRList.remove( req );
-    }
-    
-    self.WipeCRs = function () {
-      self.CRList.removeAll();
-      self.btnWipeCRs();
-    }
+    self.crl = new Page( {
+      CreateItem : function () { return new RevocationList(); }
+    } );
 
 
     /*  Helpers  */
@@ -480,7 +299,6 @@ $( function() {
       switch ( action ) {
         case 'configure':
           self.onConfigure( true );
-          self.DBList.sort( sortByName );
           break;
         case 'privatekeys':
           self.onPrivateKeys( true );
